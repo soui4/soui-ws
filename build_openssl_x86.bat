a844f9fb28ba366a21bf0143971dc9be954ef656
huangjianxiong1 <huangjianxiong1@yy.com>
2024-12-09 17:19:10 +0800

    update build_openssl_x86.bat

--- build_openssl_x86.bat
+++ build_openssl_x86.bat
@@ -5,7 +5,7 @@ set buildDir=%~dp0
 cd openssl
 mkdir build32
 cd build32
-perl ..\Configure VC-WIN32 no-shared --prefix=%buildDir%\openssl_output\x86
+perl ..\Configure VC-WIN32 --prefix=%buildDir%\openssl_output\x86_dll
 call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
 nmake
 nmake install
